5a2b97f982c61c30443403e64b880bf81bd5dca1
Mirko Rapisarda <mirkorap16@gmail.com>
2021-01-19 21:56:24 +0100

    feat: add empty components to sign in

---
+++ src/app/modules/auth/components/sign-in-form/sign-in-form.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-in-form',
+  templateUrl: './sign-in-form.component.html',
+  styleUrls: ['./sign-in-form.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SignInFormComponent {}
---
+++ src/app/modules/auth/pages/sign-in-page/sign-in-page.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-in-page',
+  templateUrl: './sign-in-page.component.html',
+  styleUrls: ['./sign-in-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SignInPageComponent {}
