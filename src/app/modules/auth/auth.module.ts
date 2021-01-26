cda46fbe46909473465f5dea6da81e853534de2e
Mirko Rapisarda <mirkorap16@gmail.com>
2021-01-26 21:25:28 +0100

    fix: import Angular Firebase modules

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,5 +1,7 @@
 import * as fromServices from './services';
 import { AuthEffects, authFeatureKey, authReducer } from './store';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireModule } from '@angular/fire';
 import { AuthRoutingModule } from './auth-routing.module';
 import { CommonModule } from '@angular/common';
 import { EffectsModule } from '@ngrx/effects';
@@ -7,14 +9,17 @@ import { NgModule } from '@angular/core';
 import { SignInFormComponent } from './components/sign-in-form/sign-in-form.component';
 import { SignInPageComponent } from './pages/sign-in-page/sign-in-page.component';
 import { StoreModule } from '@ngrx/store';
+import { environment } from '../../../environments/environment';
 
 @NgModule({
   declarations: [SignInPageComponent, SignInFormComponent],
   imports: [
     CommonModule,
     AuthRoutingModule,
+    EffectsModule.forFeature([AuthEffects]),
     StoreModule.forFeature(authFeatureKey, authReducer),
-    EffectsModule.forFeature([AuthEffects])
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule
   ],
   providers: [
     {
