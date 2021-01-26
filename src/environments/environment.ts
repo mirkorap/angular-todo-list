0a0e7b0902005793ce28345d60f73f44b68f9390
Mirko Rapisarda <mirkorap16@gmail.com>
2021-01-26 21:19:33 +0100

    chore: add firebase environment keys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,9 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDQOmbV-8qIwqoRvtvUvlHSG1HqV0_bl0M',
+    authDomain: 'note-app-97b4a.firebaseapp.com',
+    projectId: 'note-app-97b4a',
+    appId: '1:782705201213:web:a6ab4bef5b3dc797855a54'
+  }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
