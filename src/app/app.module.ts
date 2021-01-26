a25d7d8bd02d58ab858b1dc238e2cfa398507688
Mirko Rapisarda <mirkorap16@gmail.com>
2021-01-26 21:27:38 +0100

    fix: import store module and configure store devtools module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,21 @@
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
+import { EffectsModule } from '@ngrx/effects';
 import { NgModule } from '@angular/core';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreModule } from '@ngrx/store';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    EffectsModule.forRoot([]),
+    StoreModule.forRoot({}),
+    !environment.production ? StoreDevtoolsModule.instrument() : []
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
