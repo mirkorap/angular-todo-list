3b5dab788e9498cbeef8ca82f9452db128e67c3e
Mirko Rapisarda <mirkorap16@gmail.com>
2021-02-17 21:02:40 +0100

    chore: ignore environments files

--- src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: false,
-  firebase: {
-    apiKey: 'AIzaSyDQOmbV-8qIwqoRvtvUvlHSG1HqV0_bl0M',
-    authDomain: 'note-app-97b4a.firebaseapp.com',
-    projectId: 'note-app-97b4a',
-    appId: '1:782705201213:web:a6ab4bef5b3dc797855a54'
-  }
-};
