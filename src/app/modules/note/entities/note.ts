af9327260f76fbb2c55481e77ebe3ef284f1e884
Mirko Rapisarda <mirkorap16@gmail.com>
2021-02-18 20:55:27 +0100

    fix: add id field

--- src/app/modules/note/entities/note.ts
+++ src/app/modules/note/entities/note.ts
@@ -2,13 +2,23 @@ import { LimitedList } from '@shared/value-objects/limited-list';
 import { NoteBody } from '@note/value-objects/note-body';
 import { NoteColor } from '@note/value-objects/note-color';
 import { TodoItem } from './todo-item';
+import { UniqueId } from '@shared/value-objects/uuid';
 
 export class Note {
+  static MAX_TODOS_NUMBER = 3;
+
+  id: UniqueId;
   body: NoteBody;
   color: NoteColor;
   todos: LimitedList<TodoItem>;
 
-  constructor(body: NoteBody, color: NoteColor, todos: LimitedList<TodoItem>) {
+  constructor(
+    id: UniqueId,
+    body: NoteBody,
+    color: NoteColor,
+    todos: LimitedList<TodoItem>
+  ) {
+    this.id = id;
     this.body = body;
     this.color = color;
     this.todos = todos;
