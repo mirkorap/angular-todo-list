4713c74a6b2c338865d1dc9c7a72c9eef84785e6
Mirko Rapisarda <mirkorap16@gmail.com>
2021-02-25 20:43:48 +0100

    feat: add page to display notes

--- src/app/modules/note/note-routing.module.ts
+++ src/app/modules/note/note-routing.module.ts
@@ -1,7 +1,14 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { NoteOverviewPageComponent } from './pages/note-overview-page/note-overview-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: NoteOverviewPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/note/note.module.ts
+++ src/app/modules/note/note.module.ts
@@ -5,13 +5,14 @@ import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { EffectsModule } from '@ngrx/effects';
 import { NgModule } from '@angular/core';
+import { NoteOverviewPageComponent } from './pages/note-overview-page/note-overview-page.component';
 import { NoteRoutingModule } from './note-routing.module';
 import { SharedModule } from '@shared/shared.module';
 import { StoreModule } from '@ngrx/store';
 import { environment } from '@environments/environment';
 
 @NgModule({
-  declarations: [],
+  declarations: [NoteOverviewPageComponent],
   imports: [
     SharedModule,
     NoteRoutingModule,
