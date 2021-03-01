1bca9da89bc1c6d2e14b7bfebe7f286d27da193e
Mirko Rapisarda <mirkorap16@gmail.com>
2021-03-01 21:10:20 +0100

    feat: add decorator to unsubscribe from observables

---
+++ src/app/shared/decorators/auto-unsubscribe.ts
@@ -0,0 +1,14 @@
+export function AutoUnsubscribe() {
+  return function (constructor: any): void {
+    const orig = constructor.prototype.ngOnDestroy;
+    constructor.prototype.ngOnDestroy = function () {
+      for (const prop in this) {
+        const property = this[prop];
+        if (typeof property.subscribe === 'function') {
+          property.unsubscribe();
+        }
+      }
+      orig.apply();
+    };
+  };
+}
