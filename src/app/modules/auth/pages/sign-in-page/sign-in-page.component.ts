3c87dea446d079723c2a0f2c1b891cd56a4f359f
Mirko Rapisarda <mirkorap16@gmail.com>
2021-03-01 21:10:42 +0100

    feat: inject ToastrService to show error messages

--- src/app/modules/auth/pages/sign-in-page/sign-in-page.component.ts
+++ src/app/modules/auth/pages/sign-in-page/sign-in-page.component.ts
@@ -1,6 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { AuthStoreFacadeService } from '@auth/services/auth-store-facade.service';
+import { AutoUnsubscribe } from '@shared/decorators/auto-unsubscribe';
 import { ICredentialsDto } from '@auth/data-transfer-objects/credentials';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-sign-in-page',
@@ -8,8 +10,18 @@ import { ICredentialsDto } from '@auth/data-transfer-objects/credentials';
   styleUrls: ['./sign-in-page.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SignInPageComponent {
-  constructor(private authStoreFacade: AuthStoreFacadeService) {}
+@AutoUnsubscribe()
+export class SignInPageComponent implements OnInit {
+  constructor(
+    private authStoreFacade: AuthStoreFacadeService,
+    private toastr: ToastrService
+  ) {}
+
+  ngOnInit(): void {
+    this.authStoreFacade.failureMessage$.subscribe(
+      (message) => message && this.toastr.error(message)
+    );
+  }
 
   onSignIn(credentials: ICredentialsDto): void {
     this.authStoreFacade.signInWithEmailAndPassword(
