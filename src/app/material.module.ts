faf8f510261513f1ed82a63dd8050da66ff26748
Mirko Rapisarda <mirkorap16@gmail.com>
2021-04-13 20:46:32 +0200

    feat: import/export MatCheckboxModule

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,11 +1,24 @@
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { NgModule } from '@angular/core';
 
 @NgModule({
-  imports: [MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule],
-  exports: [MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule]
+  imports: [
+    MatButtonModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule
+  ]
 })
 export class MaterialModule {}
