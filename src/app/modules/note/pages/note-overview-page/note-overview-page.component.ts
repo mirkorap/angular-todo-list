9b80bda7a169c3df563b9cd3cfa19e708dd3b85b
Mirko Rapisarda <mirkorap16@gmail.com>
2021-05-04 20:33:21 +0200

    feat: implemented page to display all notes

--- src/app/modules/note/pages/note-overview-page/note-overview-page.component.ts
+++ src/app/modules/note/pages/note-overview-page/note-overview-page.component.ts
@@ -1,4 +1,6 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Note } from '@note/entities/note';
+import { NoteStoreFacadeService } from '@note/services/note-store-facade.service';
 
 @Component({
   selector: 'app-note-overview-page',
@@ -6,4 +8,26 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./note-overview-page.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class NoteOverviewPageComponent {}
+export class NoteOverviewPageComponent implements OnInit {
+  constructor(public noteStoreFacade: NoteStoreFacadeService) {}
+
+  ngOnInit(): void {
+    this.noteStoreFacade.loadAllNotes();
+  }
+
+  onUncompletedFilterChange(checked: boolean): void {
+    if (checked) {
+      return this.noteStoreFacade.loadUncompletedNotes();
+    }
+
+    return this.noteStoreFacade.loadAllNotes();
+  }
+
+  onNoteChange(note: Note): void {
+    this.noteStoreFacade.updateNote(note);
+  }
+
+  onNoteDelete(note: Note): void {
+    this.noteStoreFacade.deleteNote(note);
+  }
+}
