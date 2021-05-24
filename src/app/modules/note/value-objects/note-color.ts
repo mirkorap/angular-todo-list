f6e09e1631f1a767a8695bb9af888d8bbe314902
Mirko Rapisarda <mirkorap16@gmail.com>
2021-05-24 20:49:44 +0200

    feat: add default note color

--- src/app/modules/note/value-objects/note-color.ts
+++ src/app/modules/note/value-objects/note-color.ts
@@ -3,7 +3,7 @@ import { ValueObject } from '@shared/value-objects/value-object';
 import { validateChoice } from '@shared/validators/value-validators';
 
 export class NoteColor extends ValueObject<string> {
-  private static predefinedColors = [
+  public static PREDEFINED_COLORS = [
     '#FAFAFA', // canvas
     '#FA8072', // salmon
     '#FEDC56', // mustard
@@ -13,13 +13,15 @@ export class NoteColor extends ValueObject<string> {
     '#FFFDD0' // cream
   ];
 
+  public static DEFAULT_COLOR = NoteColor.PREDEFINED_COLORS[0];
+
   protected _value: string;
 
   constructor(noteColor: string) {
     super();
 
-    if (!validateChoice(noteColor, NoteColor.predefinedColors)) {
-      throw new InvalidChoice(NoteColor.predefinedColors);
+    if (!validateChoice(noteColor, NoteColor.PREDEFINED_COLORS)) {
+      throw new InvalidChoice(NoteColor.PREDEFINED_COLORS);
     }
 
     this._value = noteColor;
