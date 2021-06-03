abff31e115ac5177ec61ddbae7f734cb34f96a8f
Mirko Rapisarda <mirkorap16@gmail.com>
2021-06-03 20:39:20 +0200

    feat: add method to return an entity as observable

--- src/app/shared/entities/entity.ts
+++ src/app/shared/entities/entity.ts
@@ -1,7 +1,13 @@
+import { Observable, of } from 'rxjs';
+
 export abstract class Entity {
   copyWith(properties: Partial<this>): this {
     const copy = new (this.constructor as { new (): never })();
     Object.assign(copy, this, properties);
     return copy;
+  }
+
+  asObservable(): Observable<this> {
+    return of(this);
   }
 }
