1e1cff4dc1dec57f3c700f87a9b4a75da1e9a8b3
Mirko Rapisarda <mirkorap16@gmail.com>
2021-06-17 20:36:07 +0200

    feat: implement ControlValueAccessor to create a custom form control element

--- src/app/modules/note/components/todo-item/todo-item.component.ts
+++ src/app/modules/note/components/todo-item/todo-item.component.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-empty-function */
+
 import {
   ChangeDetectionStrategy,
   Component,
@@ -5,20 +8,77 @@ import {
   Input,
   Output
 } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { MatCheckboxChange } from '@angular/material/checkbox';
 import { TodoItem } from '@note/entities/todo-item';
 import { TodoName } from '@note/value-objects/todo-name';
+
+type OnChange = (todo: TodoItem) => void;
+type OnTouched = () => void;
 
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
   styleUrls: ['./todo-item.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: TodoItemComponent
+    }
+  ]
 })
-export class TodoItemComponent {
+export class TodoItemComponent implements ControlValueAccessor {
   @Input() todo!: TodoItem;
   @Output() todoRemove = new EventEmitter<TodoItem>();
 
   nameMaxLength = TodoName.MAX_LENGTH;
+  touched = false;
+  disabled = false;
+
+  onChange: OnChange = (_todo: TodoItem): void => {};
+  onTouched: OnTouched = () => {};
+
+  onDoneChange(event: MatCheckboxChange): void {
+    this.markAsTouched();
+    if (!this.disabled) {
+      this.todo = this.todo.copyWith({ done: event.checked });
+      this.onChange(this.todo);
+    }
+  }
+
+  onNameInput(event: Event): void {
+    this.markAsTouched();
+    if (!this.disabled) {
+      const name = (event.target as HTMLInputElement).value;
+      this.todo = this.todo.copyWith({ name: new TodoName(name) });
+      this.onChange(this.todo);
+    }
+  }
+
+  writeValue(todo: TodoItem): void {
+    this.todo = todo;
+  }
+
+  registerOnChange(onChange: OnChange): void {
+    this.onChange = onChange;
+  }
+
+  registerOnTouched(onTouched: OnTouched): void {
+    this.onTouched = onTouched;
+  }
+
+  markAsTouched(): void {
+    if (!this.touched) {
+      this.onTouched();
+      this.touched = true;
+    }
+  }
+
+  setDisabledState(disabled: boolean): void {
+    this.disabled = disabled;
+  }
 
   onRemoveClick(): void {
     this.todoRemove.emit(this.todo);
